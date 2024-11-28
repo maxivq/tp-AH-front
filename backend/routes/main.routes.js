196b839d5abf9775443e8a71de1625e87480b2b6
maxivq <kitvelazquez@gmail.com>
2024-11-28 08:48:07 -0300

    agregando backend

--- backend/routes/main.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import mainController from "../controllers/main.controller.js";
-
-const router = Router();
-
-router.use("/", mainController);
-
-export default router;
